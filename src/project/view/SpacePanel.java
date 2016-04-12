83b1726c1b99f68dbc0fdecbae334cf39290fd61
bhofmt <thierry.hofmann@wobo-campus.ch>
2016-04-12 14:53:29 +0200

    Alien Picture displayed

--- src/project/view/SpacePanel.java
+++ src/project/view/SpacePanel.java
@@ -4,7 +4,14 @@ import java.awt.Color;
 import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 import project.controller.SpaceController;
@@ -17,46 +24,57 @@ public class SpacePanel extends JPanel
 	private static final long serialVersionUID = -3891267326305936579L;
 	private SpaceController controller;
 	private String gameOverText = "";
-	
-	public SpacePanel ( )
+	private Image alien;
+
+	public SpacePanel ()
 	{
-		controller = SpaceController.getInstanceOf ( );
+		controller = SpaceController.getInstanceOf ();
 		setFocusable ( true );
 		requestFocusInWindow ( true );
+
+		alien = readImage ( "images/alien.png" );
 	}
-	
+
 	@Override
-	protected void paintComponent ( Graphics g )
+	protected void paintComponent(Graphics g)
 	{
-		Graphics2D g2d = ( Graphics2D ) g;
+		Graphics2D g2d = (Graphics2D) g;
 		super.paintComponent ( g2d );
-		
-		ArrayList<Enemy> enemies = controller.getEnemies ( );
-		ArrayList<Projectile> projectiles = controller.getProjectiles ( );
+
+		ArrayList<Enemy> enemies = controller.getEnemies ();
+		ArrayList<Projectile> projectiles = controller.getProjectiles ();
 		ArrayList<Projectile> cloneProjectiles = controller.cloneArrayList ( projectiles );
 		ArrayList<Enemy> cloneEnemies = controller.cloneArrayList ( enemies );
-		
-		Player player = controller.getPlayer ( );
-		
+
+		Player player = controller.getPlayer ();
+
 		// Draw all enemies
 		g2d.setColor ( Color.RED );
 		for ( Enemy enemy : enemies )
 		{
-			g2d.fillRect ( ( int ) enemy.getX ( ), ( int ) enemy.getY ( ), ( int ) enemy.getWidth ( ), ( int ) enemy.getHeight ( ) );
+//			g2d.fillRect ( (int) enemy.getX (), (int) enemy.getY (), (int) enemy.getWidth (),
+//					(int) enemy.getHeight () );
+
+			g2d.drawImage ( alien, (int) enemy.getX (), (int) enemy.getY (), (int) ( alien.getWidth ( null ) / 3 ),
+					(int) ( alien.getHeight ( null ) / 3 ), null );
+
+			enemy.height = (int) ( alien.getHeight ( null ) / 3 );
+			enemy.width = (int) ( alien.getWidth ( null ) / 3 );
 		}
-		
+
 		// Draw the Player.
 		g2d.setColor ( Color.BLUE );
-		g2d.fillRect ( ( int ) player.getX ( ), ( int ) player.getY ( ), ( int ) player.getWidth ( ), ( int ) player.getHeight ( ) );
-		
+		g2d.fillRect ( (int) player.getX (), (int) player.getY (), (int) player.getWidth (),
+				(int) player.getHeight () );
+
 		g2d.setColor ( Color.RED );
-		
+
 		for ( Projectile projectile : projectiles )
 		{
-			g2d.fillRect (	( int ) projectile.getX ( ), ( int ) projectile.getY ( ), ( int ) projectile.getWidth ( ),
-							( int ) projectile.getHeight ( ) );
+			g2d.fillRect ( (int) projectile.getX (), (int) projectile.getY (), (int) projectile.getWidth (),
+					(int) projectile.getHeight () );
 		}
-		
+
 		for ( Projectile projectile : cloneProjectiles )
 		{
 			for ( Enemy enemy : cloneEnemies )
@@ -69,31 +87,56 @@ public class SpacePanel extends JPanel
 				}
 			}
 		}
-		
+
 		g2d.setColor ( Color.WHITE );
-		
+
 		int stringWidth = 0;
 		int stringAccent = 0;
 		int xCoordinate = 0;
 		int yCoordinate = 0;
 		// Change the fontSize.
-		g2d.setFont(g.getFont().deriveFont(20F));
+		g2d.setFont ( g.getFont ().deriveFont ( 20F ) );
 		// get the FontMetrics for the current font
-		FontMetrics fm = g2d.getFontMetrics ( );
-		
+		FontMetrics fm = g2d.getFontMetrics ();
+
 		/** Centering the text */
 		// find the center location to display
 		stringWidth = fm.stringWidth ( gameOverText );
-		stringAccent = fm.getAscent ( );
+		stringAccent = fm.getAscent ();
 		// get the position of the leftmost character in the baseline
-		xCoordinate = getWidth ( ) / 2 - stringWidth / 2;
-		yCoordinate = getHeight ( ) / 2 + stringAccent / 2;
-		
+		xCoordinate = getWidth () / 2 - stringWidth / 2;
+		yCoordinate = getHeight () / 2 + stringAccent / 2;
+
 		// draw String
 		g2d.drawString ( gameOverText, xCoordinate, yCoordinate );
 	}
-	
-	public void setGameOverText ( String s )
+
+	private Image readImage(String path)
+	{
+		InputStream stream;
+		BufferedImage image = null;
+
+		stream = this.getClass ().getClassLoader ().getResourceAsStream ( path );
+
+		try
+		{
+			image = ImageIO.read ( stream );
+		}
+		catch ( IOException e )
+		{
+			System.err.println ( "Couldn't load the Texture! Cause: " + e.getMessage () );
+			System.exit ( 67 );
+		}
+		catch ( IllegalArgumentException e )
+		{
+			System.err.println ( "Couldn't read the Texture! Cause: " + e.getMessage () );
+			System.exit ( 68 );
+		}
+
+		return image;
+	}
+
+	public void setGameOverText(String s)
 	{
 		gameOverText = s;
 	}
