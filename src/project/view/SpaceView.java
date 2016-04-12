05b9cb8742b8cde33b45de399d23ad44b0ff3d2a
bhofmt <thierry.hofmann@wobo-campus.ch>
2016-04-12 17:13:08 +0200

    Test Button added

--- src/project/view/SpaceView.java
+++ src/project/view/SpaceView.java
@@ -2,8 +2,11 @@ package project.view;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.GraphicsEnvironment;
+import java.awt.Insets;
 import java.awt.Point;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -14,7 +17,9 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.border.Border;
 
+import javafx.scene.control.ComboBox;
 import project.controller.SpaceController;
 import project.model.Player;
 import project.thread.Time;
@@ -22,194 +27,207 @@ import project.thread.Time;
 public class SpaceView extends JFrame
 {
 	private static final long serialVersionUID = 1L;
-	
-	private static final JLabel levelStatus = new JLabel ( );
-	private static final JLabel cannonStatus = new JLabel ( );
-	
+
+	private static final JLabel levelStatus = new JLabel ();
+	private static final JLabel cannonStatus = new JLabel ();
+
 	private static SpaceView gui = null;
 	private static SpaceController controller = null;
 	private static SpacePanel gamePanel = null;
-	
-	private SpaceView ( )
+
+	private SpaceView ()
 	{
-	
+
 	}
-	
-	public static SpaceView getInstanceOf ( )
+
+	public static SpaceView getInstanceOf()
 	{
 		if ( gui == null )
 		{
-			gui = new SpaceView ( );
+			gui = new SpaceView ();
 			return gui;
 		}
-		
+
 		else
 		{
 			return gui;
 		}
 	}
-	
-	private static void centerFrameAndMakeItVisible ( JFrame frame )
+
+	private static void centerFrameAndMakeItVisible(JFrame frame)
 	{
-		Dimension windowSize = frame.getSize ( );
-		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment ( );
-		Point centerPoint = ge.getCenterPoint ( );
-		
+		Dimension windowSize = frame.getSize ();
+		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment ();
+		Point centerPoint = ge.getCenterPoint ();
+
 		int dx = centerPoint.x - windowSize.width / 2;
 		int dy = centerPoint.y - windowSize.height / 2;
-		
+
 		frame.setLocation ( dx, dy );
 		frame.setVisible ( true );
 		frame.setResizable ( false );
 	}
-	
-	private static void addKeyListener ( )
+
+	private static void addKeyListener()
 	{
-		KeyListener keyListener = new KeyListener ( )
+		KeyListener keyListener = new KeyListener ()
 		{
-			private SpaceController ctrl = SpaceController.getInstanceOf ( );
-			
+			private SpaceController ctrl = SpaceController.getInstanceOf ();
+
 			@Override
-			public void keyTyped ( KeyEvent e )
+			public void keyTyped(KeyEvent e)
 			{
 				// Do nothing.
 			}
-			
+
 			@Override
-			public void keyPressed ( KeyEvent e )
+			public void keyPressed(KeyEvent e)
 			{
-				switch ( e.getKeyCode ( ) )
+				switch ( e.getKeyCode () )
 				{
-				case KeyEvent.VK_LEFT:
-				case KeyEvent.VK_A:
-					ctrl.movePlayerLeft ( true );
+					case KeyEvent.VK_LEFT:
+					case KeyEvent.VK_A:
+						ctrl.movePlayerLeft ( true );
 					break;
-				case KeyEvent.VK_RIGHT:
-				case KeyEvent.VK_D:
-					ctrl.movePlayerRight ( true );
+					case KeyEvent.VK_RIGHT:
+					case KeyEvent.VK_D:
+						ctrl.movePlayerRight ( true );
 					break;
-				case KeyEvent.VK_SPACE:
-					ctrl.makePlayerShoot ( );
+					case KeyEvent.VK_SPACE:
+						ctrl.makePlayerShoot ();
 					break;
 				}
 			}
-			
+
 			@Override
-			public void keyReleased ( KeyEvent e )
+			public void keyReleased(KeyEvent e)
 			{
-				switch ( e.getKeyCode ( ) )
+				switch ( e.getKeyCode () )
 				{
-				case KeyEvent.VK_LEFT:
-				case KeyEvent.VK_A:
-					ctrl.movePlayerLeft ( false );
+					case KeyEvent.VK_LEFT:
+					case KeyEvent.VK_A:
+						ctrl.movePlayerLeft ( false );
 					break;
-				case KeyEvent.VK_RIGHT:
-				case KeyEvent.VK_D:
-					ctrl.movePlayerRight ( false );
+					case KeyEvent.VK_RIGHT:
+					case KeyEvent.VK_D:
+						ctrl.movePlayerRight ( false );
 					break;
 				}
 			}
-			
+
 		};
-		
+
 		gamePanel.addKeyListener ( keyListener );
 	}
-	
-	private static void addMainMenu ( )
+
+	private static void addMainMenu()
 	{
-		gui.add ( new JButton ( "Peschä" ) );
-		
+		// JPanel
+		JPanel pnlButton = new JPanel ();
+		// Buttons
+		JButton btnPeschä = new JButton ( "Peschä" );
+
+		// FlightInfo setbounds
+		btnPeschä.setBounds ( 60, 400, 220, 30 );
+
+		// JPanel bounds
+		pnlButton.setBounds ( 800, 800, 200, 100 );
+
+		// Adding to JFrame
+		pnlButton.add ( btnPeschä );
+		gui.add ( pnlButton );
+
 		gui.validate ();
 		gui.repaint ();
 	}
-	
-	private static void addGameEnvironment ( )
+
+	private static void addGameEnvironment()
 	{
-		
-		controller = SpaceController.getInstanceOf ( );
-		
-		JPanel borderPanel = new JPanel ( new BorderLayout ( ) );
-		JPanel userInterface = new JPanel ( );
-		Dimension expectedDimension = new Dimension ( gui.getWidth ( ) - 100, gui.getHeight ( ) - 100 );
-		gamePanel = new SpacePanel ( );
-		
+
+		controller = SpaceController.getInstanceOf ();
+
+		JPanel borderPanel = new JPanel ( new BorderLayout () );
+		JPanel userInterface = new JPanel ();
+		Dimension expectedDimension = new Dimension ( gui.getWidth () - 100, gui.getHeight () - 100 );
+		gamePanel = new SpacePanel ();
+
 		borderPanel.setPreferredSize ( expectedDimension );
 		borderPanel.setMaximumSize ( expectedDimension );
 		borderPanel.setMinimumSize ( expectedDimension );
-		
+
 		borderPanel.setBackground ( Color.RED );
-		
+
 		Box box = new Box ( BoxLayout.Y_AXIS );
-		
-		box.add ( Box.createVerticalGlue ( ) );
+
+		box.add ( Box.createVerticalGlue () );
 		box.add ( borderPanel );
-		box.add ( Box.createVerticalGlue ( ) );
+		box.add ( Box.createVerticalGlue () );
 		box.setBackground ( Color.GRAY );
 		box.setOpaque ( true );
-		
+
 		userInterface.setBackground ( Color.BLACK );
-		userInterface.setPreferredSize ( new Dimension ( borderPanel.getWidth ( ), 100 ) );
-		
+		userInterface.setPreferredSize ( new Dimension ( borderPanel.getWidth (), 100 ) );
+
 		gamePanel.setBackground ( Color.DARK_GRAY );
-		
+
 		cannonStatus.setForeground ( Color.WHITE );
 		userInterface.add ( cannonStatus );
-		
+
 		levelStatus.setForeground ( Color.WHITE );
 		userInterface.add ( levelStatus );
-		
+
 		borderPanel.add ( gamePanel, BorderLayout.CENTER );
 		borderPanel.add ( userInterface, BorderLayout.NORTH );
-		
+
 		gui.add ( box, BorderLayout.CENTER );
 
-		Time timemaker = new Time ( );
+		Time timemaker = new Time ();
 		controller.setTimer ( timemaker );
-		
-		timemaker.addListener ( controller );
-		timemaker.start ( );
-		
-		controller.setPlayer ( new Player ( 0, SpaceController.getGamePanel ( ).getHeight ( ) - 50 ) );
 
-		addKeyListener ( );
+		timemaker.addListener ( controller );
+		timemaker.start ();
+
+		controller.setPlayer ( new Player ( 0, SpaceController.getGamePanel ().getHeight () - 50 ) );
+
+		addKeyListener ();
 	}
-	
-	private static void initiateGui ( )
+
+	private static void initiateGui()
 	{
-		gui = getInstanceOf ( );
-		
-		gui.setLayout ( new BorderLayout ( ) );
+		gui = getInstanceOf ();
+
+		gui.setLayout ( new BorderLayout () );
 		gui.setSize ( 1200, 800 );
 		gui.setTitle ( "Space Invaders" );
 		gui.setDefaultCloseOperation ( EXIT_ON_CLOSE );
 		gui.setAutoRequestFocus ( false );
 		gui.setResizable ( false );
-		
+
 		centerFrameAndMakeItVisible ( gui );
-		
-		gui.validate ( );
-		gui.repaint ( );
+
+		gui.validate ();
+		gui.repaint ();
 	}
-	
-	public void changeLevelStatusText ( )
+
+	public void changeLevelStatusText()
 	{
-		levelStatus.setText ( "<html>Level: " + controller.getLevel ( ) + "</html>" );
+		levelStatus.setText ( "<html>Level: " + controller.getLevel () + "</html>" );
 	}
-	
-	public void changeCannonStatusText ( String s )
+
+	public void changeCannonStatusText(String s)
 	{
 		cannonStatus.setText ( s );
 	}
-	
-	public static void main ( String[] args )
+
+	public static void main(String[] args)
 	{
-		initiateGui ( );
-		addMainMenu ( );
+		initiateGui ();
+		addMainMenu ();
 	}
-	
-	public static SpacePanel getGamePanel ( )
+
+	public static SpacePanel getGamePanel()
 	{
 		return gamePanel;
 	}
-	
+
 }
